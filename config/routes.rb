12939312d009860f97c62c09a9344d89e27dea6b
Anthony Tokatly <atokatly884@gmail.com>
2017-02-27 16:00:03 -0800

    add functionality to create a business and create a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "users#index"
-  resources :users  
+  resources :users
+  resources :businesses 
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
