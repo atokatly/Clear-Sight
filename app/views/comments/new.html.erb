19820a1fabcd7215c543f54e9521509fae403ce7
Anthony Tokatly <atokatly884@gmail.com>
2017-03-14 13:35:57 -0700

    add comments routes

---
+++ app/views/comments/new.html.erb
@@ -0,0 +1,1 @@
+<h1>Create new comment</h1>
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :users
   resources :businesses
   resources :jobs do
-    resources :tasks
+    resources :tasks do
+      resources :comments, shallow: true 
+    end
   end
 
 
