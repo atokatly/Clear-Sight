9a5c07654230403f4dd1051cb42639b75677a8e7
Anthony Tokatly <atokatly884@gmail.com>
2017-03-14 11:49:10 -0700

    add current tasks to business show page

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -14,9 +14,7 @@ class BusinessesController < ApplicationController
     end
     if params[:business][:users][:company] == ""
       business = Business.find_by(company: params[:business][:company])
-      # p business.id
       @user = User.new(username: params[:business][:users][:username], email: params[:business][:users][:email], password: params[:business][:users][:password], password_confirmation: params[:business][:users][:password_confirmation], f_name: params[:business][:users][:f_name], l_name: params[:business][:users][:l_name], phone: params[:business][:users][:phone], title: params[:business][:users][:title], business_id: business.id)
-      # p @user.business_id = business.id
     else
       @user = User.new(username: params[:business][:users][:username], email: params[:business][:users][:email], password: params[:business][:users][:password], password_confirmation: params[:business][:users][:password_confirmation], f_name: params[:business][:users][:f_name], l_name: params[:business][:users][:l_name], phone: params[:business][:users][:phone], title: params[:business][:users][:title], business_id: params[:business][:users][:company])
     end
@@ -30,6 +28,13 @@ class BusinessesController < ApplicationController
 
   def show
     @business = Business.find_by(id: params[:id])
+    # Refactor and use code below to limit calls to db at scale
+    # @user = User.find(id).includes(:posts => :comments)
+    @users = @business.users
+    # Refactor to have a class method handle this
+    array = []
+    @users.each {|user| array << user.id}
+    @tasks = Task.where(user_id: array)
   end
 
   def edit
@@ -41,7 +46,7 @@ class BusinessesController < ApplicationController
   end
 
   def destroy
-    
+
   end
 
   private
